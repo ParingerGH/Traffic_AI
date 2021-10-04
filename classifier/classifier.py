ba9cd348a7496f0a0504891327f1e4db59654c77
Vyacheslav <gravitycat35@gmail.com>
2021-10-04 16:29:42 +0300

    Update classifier.py

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -22,6 +22,7 @@ def model_load():
   model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
   model.add(MaxPool2D(pool_size=(2, 2)))
   model.add(Dropout(rate=0.25))
+  
   model.add(Flatten())
 
   model.add(Dense(256, activation='relu'))
