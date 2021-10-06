7dda9e5683606d58cb244aa08a31edef93aa01b0
Vyacheslav <gravitycat35@gmail.com>
2021-10-06 23:27:06 +0300

    styles + scripts

--- nodejs/server.js
+++ nodejs/server.js
@@ -7,6 +7,9 @@ const PORT = 80;
 const HOST = '0.0.0.0';
 
 const app = express();
+
+app.use(express.static(__dirname + '/express'));
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/express/index.html');
 });
