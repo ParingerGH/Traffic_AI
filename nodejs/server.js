10d512652dee4af7963af1d43e18525642c4561d
ParingerGH <61421117+ParingerGH@users.noreply.github.com>
2021-10-07 12:00:37 +0300

    added app.get() styles path to server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -8,6 +8,8 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
+// many app.get() sblocks probobly can be fixed by using app.use but didn't worck for me
+
 app.get('/',(request, response) => {
     response.sendFile(__dirname + '/index.html');
 });
@@ -18,6 +20,10 @@ app.get('/script.js',(request, response) => {
 
 app.get('/applicationdata/model.txt',(request, response) => {
     response.sendFile(__dirname + '/applicationdata/model.txt');
+});
+
+app.get('/styles/styles.css',(request, response) => {
+    response.sendFile(__dirname + '/styles/styles.css');
 });
 
 
