e5df55a42665bff6a25268bf4aa5814dc0a5c0a6
ParingerGH <61421117+ParingerGH@users.noreply.github.com>
2021-10-31 23:00:42 +0300

    Update йогурт

--- server/start_server.py
+++ server/start_server.py
@@ -1,10 +1,6 @@
 import socket
 import subprocess
 
-
-print('SERVER STARTED')
-
-
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(1)
@@ -17,6 +13,4 @@ while True:
         if data == b'???':
             subprocess.run(["bash","test_script.sh"])
        
-    
-
 conn.close()
